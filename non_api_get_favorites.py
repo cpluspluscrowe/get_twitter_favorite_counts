84b816a238c2fc29e830cb367f390c4540e4cf68
Chad Crowe <ccrowe@ccrowe-mn1.linkedin.biz>
2020-02-22 17:42:10 -0800

    storing data into a database.

--- non_api_get_favorites.py
+++ non_api_get_favorites.py
@@ -66,6 +66,70 @@ def get_data(url):
     print(len(to_return[0]), len(to_return[1]))
     return to_return
 
-url = "https://twitter.com/PLAYRgg/status/1230628361984233474"
-likes, retweets = get_data(url)
-        
+#url = "https://twitter.com/PLAYRgg/status/1230628361984233474"
+#likes, retweets = get_data(url)
+
+
+# get tweets from the brand's page
+driver = login_to_twitter()
+url = "https://twitter.com/rishqo"
+
+# ---- There are certain signs that you are too cool for Python
+import sys
+sys.setrecursionlimit(100000)
+# ----
+
+def get_tweet_ids(url, old_height, count, collected_tweet_ids):
+    if old_height == 0:
+        driver.get(url)
+        time.sleep(1)
+    driver.execute_script("window.scrollBy(0,2000)")
+    time.sleep(0.3)
+    height = driver.execute_script("return document.body.scrollHeight;")
+    elements = driver.find_elements_by_xpath("//div[@data-testid='tweet']//a[contains(@href,'/{0}/status/')]".format(brand))
+    for element in elements:
+        tweet_id = element.get_attribute("href").split("/")[-1:][0]
+        collected_tweet_ids.append(tweet_id)
+    print(len(collected_tweet_ids))
+    if len(collected_tweet_ids) > 50:
+        return collected_tweet_ids
+    if height == old_height and count > 5:
+        return collected_tweet_ids
+    if height == old_height and count < 5:
+        new_count = count + 1
+        return get_tweet_ids(url, height, new_count, collected_tweet_ids)
+    else:
+        return get_tweet_ids(url, height, 1, collected_tweet_ids)
+
+import sqlite3
+def create_tweet_id_table():
+    conn = sqlite3.connect('./usernames.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE tweets
+             (brandId text, tweetId text)''')
+
+def insert_tweet_id(brand_id, tweet_id):
+    conn = sqlite3.connect('./usernames.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO tweets(brandId, tweetId) VALUES ('{0}','{1}')".format(brand_id, tweet_id))
+    conn.commit()
+    conn.close()
+
+def get_and_store_brand_tweet_ids(brand):
+    url = "https://twitter.com/{0}".format(brand)
+    tweet_ids = get_tweet_ids(url, 0, 0, [])
+    for tweet_id in tweet_ids:
+        insert_tweet_id(brand, tweet_id)
+
+def get_brand_data():
+    conn = sqlite3.connect('./usernames.db')
+    cursor = conn.execute("select brand from brands")
+    l = []
+    for row in cursor:
+        l.append(row[0])
+    conn.close()
+    return l
+
+existing = get_brand_data()
+for brand in existing:
+    get_and_store_brand_tweet_ids(brand)
--- py_api.py
+++ py_api.py
@@ -6,20 +6,123 @@ def get_keys():
 consumer_key, consumer_secret, access_token, access_token_secret = get_keys()
     
 import tweepy
-
-#access_token_secret = get_access_token()
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
-nike = ""
-
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 # Gives anything on timeline, e.g. replies
-result = api.user_timeline("Nike",count=1)
+#result = api.user_timeline("Nike",count=3)
+#brand = "Nike"
 
-# Gives the post info
-result = api.get_status("1225203298443153409")
+def get_post_data(tweet_id):
+    status = api.get_status(tweet_id)
+    favorite_count = status.favorite_count
+    retweet_count = status.retweet_count
+    created_at = status.created_at
+    return (tweet_id, favorite_count, retweet_count, created_at)
 
+
+tweet_id_old = "1127574455494987777"
+result = get_post_data(tweet_id_old)
+
+
+# The next section is about finding twitter brand pages
+
+# from http://www.opsig.org/reso/inddb/
+def insert_brand_data(brand_id):
+    conn = sqlite3.connect('./usernames.db')
+    conn.execute("INSERT INTO brands(brand) VALUES ('{0}')".format(brand_id))
+    conn.commit()
+    conn.close()
+
+def insert_follower_data(brand, follower_id):
+    conn = sqlite3.connect('./usernames.db')
+    conn.execute("INSERT INTO follower(brand, followerId) VALUES ('{0}', '{1}')".format(brand, follower_id))
+    conn.commit()
+    conn.close()
+    
+def get_brand_data():
+    conn = sqlite3.connect('./usernames.db')
+    cursor = conn.execute("select brand from brands")
+    l = []
+    for row in cursor:
+        l.append(row[0])
+    conn.close()
+    return l
+    
+industries = list(map(lambda x: x.split("\t")[1],
+                      open("./industries.txt","r").read().split("\n")[:-1]
+))
+existing = get_brand_data()
+for industry in industries:
+    if industry in existing:
+        continue # skip if we have already processed the industry
+    found = api.search(industry)
+    for tweet in found:
+        author = tweet.author.screen_name
+        print(author)
+        insert_brand_data(author)
+        print("processed")
+
+import sqlite3
+def create_follower_table():
+    conn = sqlite3.connect('./usernames.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE followers
+             (brand text, followerId integer)''')
+
+def create_brand_table():
+    conn = sqlite3.connect('./usernames.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE brands
+             (brand text)''')
+
+def create_username_table():
+    conn = sqlite3.connect('./usernames.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE usernames
+             (brand text, tweetId integer, favoriteUsername text, retweetUsername text)''')
+
+def insert_username_data(brand_id, tweet_id, favorite_username, retweet_username):
+    conn = sqlite3.connect('./usernames.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO usernames VALUES ({0},{1},{2},{3})".format(brand_id, tweet_id, favorite_username, retweet_username))
+    conn.commit()
+    conn.close()
+
+def delete_brand(to_remove):
+    conn = sqlite3.connect('./usernames.db')
+    cursor = conn.execute("delete from brands where brand = '{0}'".format(to_remove))
+    conn.commit()
+    conn.close()
+    
+# Look into api.followers
+# followers_ids
+# get_user
+def get_user(name):
+    try:
+        return api.get_user(name)
+    except Exception as e:
+        print(str(e))
+        return None
+
+def remove_non_brands(): # We end up with around 400 brand pages, from 27k usernames
+    existing = get_brand_data()
+    for brand in existing: 
+        user = get_user(brand)
+        if user:
+            follower_ratio = user.followers_count / max(1, user.friends_count)
+            if follower_ratio < 5 and user.followers_count > 300:
+                delete_brand(brand) # deletes from database
+        else:
+            delete_brand(brand)
+                
+
+# followers_count
+# friends_count
+# statuses_count
+# friends are those they are following, followers are those following.
+# we want followers / friends > 5
+
+result = api.followers_ids(brand)
+insert_follower_data
 
