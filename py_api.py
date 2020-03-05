e23e6aea277287dcc087149c7bf0dbe9e48c4bef
Chad Crowe <ccrowe@ccrowe-mn1.linkedin.biz>
2020-03-04 17:46:17 -0800

    getting types of media in each tweet

--- py_api.py
+++ py_api.py
@@ -8,7 +8,7 @@ consumer_key, consumer_secret, access_token, access_token_secret = get_keys()
 import tweepy
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, , tweet_mode="extended")
 # Gives anything on timeline, e.g. replies
 #result = api.user_timeline("Nike",count=3)
 #brand = "Nike"
@@ -20,10 +20,8 @@ def get_post_data(tweet_id):
     created_at = status.created_at
     return (tweet_id, favorite_count, retweet_count, created_at)
 
-
-tweet_id_old = "1127574455494987777"
-result = get_post_data(tweet_id_old)
-
+#tweet_id_old = "1127574455494987777"
+#result = get_post_data(tweet_id_old)
 
 # The next section is about finding twitter brand pages
 
@@ -116,13 +114,71 @@ def remove_non_brands(): # We end up with around 400 brand pages, from 27k usern
         else:
             delete_brand(brand)
                 
+#result = api.followers_ids(brand)
+#insert_follower_data
 
-# followers_count
-# friends_count
-# statuses_count
-# friends are those they are following, followers are those following.
-# we want followers / friends > 5
+def get_tweet(tweet_id):
+    tweet = api.get_status(tweet_id)
+    return tweet
 
-result = api.followers_ids(brand)
-insert_follower_data
+def does_tweet_contain_image(tweet):
+    if "media" in tweet.entities:
+        for entity in tweet.entities["media"]:
+            if entity["type"] == "photo":
+                return True
+    return False
+
+def does_tweet_contain_urls(tweet):
+    if "urls" in tweet.entities:
+        if tweet.entities["urls"]:
+            return True # can have key without any data
+        else:
+            return False
+    else:
+        return False
+
+def does_tweet_contain_text(tweet):
+    if tweet.full_text:
+        return True
+    else:
+        return False
+
+def get_tweet_data(tweet_id):
+    tweet = api.get_status(tweet_id, include_ext_alt_text = True,tweet_mode="extended")
+    has_image = does_tweet_contain_image(tweet)
+    has_urls = does_tweet_contain_urls(tweet)
+    has_text = does_tweet_contain_text(tweet)
+    return (has_text, has_image, has_urls)
+
+# url
+#tweet.text
+
+def create_media_table():
+    conn = sqlite3.connect('./usernames.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE media
+             (tweetId integer, hasText integer, hasImage integer, hasUrl integer)''')
+
+def insert_tweet_id(tweet_id,has_text, has_image,has_url):
+    conn = sqlite3.connect('./usernames.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO media(tweetId, hasText, hasImage, hasUrl) VALUES ('{0}','{1}','{2}','{3}')".format(tweet_id,has_text, has_image,has_url))
+    conn.commit()
+    conn.close()
+
+def get_tweet_ids():
+    conn = sqlite3.connect('./usernames.db')
+    cursor = conn.execute("select brandId, tweetId from tweets")
+    l = []
+    for tweet_id in cursor:
+        l.append(row)
+    conn.close()
+    return l
+
+tweet_ids = get_tweet_ids()
+for brand, tweet_id in tweet_ids:
+    print(brand, tweet_id)
+    has_text, has_image, has_url = get_tweet_data(tweet_id)
+    insert_tweet_id(tweet_id, has_text, has_image, has_url)
+    break
 
